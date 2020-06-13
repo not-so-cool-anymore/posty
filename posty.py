b8c78f6113f8e864d12f30f5618aee5f878bded9
Ivan Zlatanov <ivanff1@abv.bg>
2020-06-13 18:57:28 +0300

    Regex URL validation was added.

--- posty.py
+++ posty.py
@@ -2,25 +2,29 @@
 
 import urllib.request
 import urllib.error
+import re
 import clipboard
     
 class Posty():
     def __init__(self):
+        self.regex_url_pattern = r"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w\-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)"
         self.content = clipboard.paste()
         self.shortener_api_url = "http://tinyurl.com/api-create.php?url=" 
     
     def shorten(self):
         request_link = self.shortener_api_url + self.content
         
-        try:
-            raw_shortened_url = urllib.request.urlopen(request_link).read()
-            decoded_shortened_url = raw_shortened_url.decode('utf-8')
+        if re.match(self.regex_url_pattern, self.content):
+            try:
+                raw_shortened_url = urllib.request.urlopen(request_link).read()
+                decoded_shortened_url = raw_shortened_url.decode('utf-8')
 
-            print(decoded_shortened_url)
+                print(decoded_shortened_url)
 
-        except urllib.error.HTTPError:
-            print('An error ocurred.')
-            print('No internet connection or invalid link.')
+            except urllib.error.HTTPError:
+                print('An HTTP error ocurred.')
+        else:
+            print('Error: Invalid link.')
 
 if __name__=="__main__":
     Posty().shorten()
